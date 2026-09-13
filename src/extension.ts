39003db86bffaf649761fedb07d8a662cb70c3d0
nlabrazi <na.labrazi@gmail.com>
2026-09-13 09:31:41 +0200

    feat: rename command and add status bar indicator
    - Renamed command from 'nexus.helloWorld' to 'nexus.status'
    - Updated command title to 'Nexus: Status'
    - Implemented status bar item with Nexus indicator
    - Added workspace information to status message
    - Replaced old extension code with new implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const command = vscode.commands.registerCommand('nexus.status', () => {
+    const workspace = vscode.workspace.workspaceFolders?.[0];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "nexus" is now active!');
+    const workspaceName = workspace?.name ?? 'No workspace';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('nexus.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Nexus!');
-	});
+    vscode.window.showInformationMessage(
+      `Nexus active — Workspace: ${workspaceName}`
+    );
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(command);
+
+  const statusBar = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+
+  statusBar.text = '$(radio-tower) Nexus: ON';
+  statusBar.tooltip = 'Nexus is running';
+  statusBar.command = 'nexus.status';
+
+  statusBar.show();
+
+  context.subscriptions.push(statusBar);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
